be6cf5502d3916f502cbb923ea46852dcf2fce5a
orakili <jo@orakili.net>
2014-01-26 18:18:22 +0900

    first attempt Philippines

---
+++ phl2/js/leaflet.topojson.js
@@ -0,0 +1,16 @@
+ (function() {
+  L.TopoJSON = L.GeoJSON.extend({
+    addData: function(jsonData) {
+      if (jsonData.type === "Topology") {
+        for (key in jsonData.objects) {
+          geojson = topojson.feature(jsonData, jsonData.objects[key]);
+          L.GeoJSON.prototype.addData.call(this, geojson);
+        }
+      }
+      else {
+        L.GeoJSON.prototype.addData.call(this, jsonData);
+      }
+      return this;
+    }
+  });
+ })();
